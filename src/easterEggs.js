5820d9adeebee8c4e9bec3f81ec0dd716eee9bbb
gcq <gcquiroga@gmail.com>
2014-06-07 14:54:28 +0200

    Afegit easter egg per als sants innocents

--- src/easterEggs.js
+++ src/easterEggs.js
@@ -56,16 +56,58 @@ function catalonia () {
     flag(30, 0, 30, window.screen.height).style.transform = "rotate(90deg)";
 }
 
+function aprilFools () {
+    // https://github.com/seutje/aprilFools.css
+    var _default = "@keyframes spin{0%{transform:rotate(0deg);}100%{transform:rotate(360deg);}}@keyframes fall{0%{transform:none;}100%{transform:rotateX(-90deg);}}",
+        
+        image_flip = "img{transform:rotate(180deg);}",
+        spin_image = "img {animation: spin 1s linear infinite;}",
+        comic_sans = "* {font-family: 'Comic Sans MS', cursive !important;}",
+        foggy_text = "* {text-shadow: 0 0 0.3em rgba(0, 0, 0, 1);}",
+        invisible_selection = "::-moz-selection{color:rgba(0, 0, 0, 0);}::selection{color:rgba(0, 0, 0, 0);}",
+
+        upside_down = "body{transform:rotate(180deg);}",
+        fall = "html, body {height: 100%;}html {-moz-perspective: 1000;}body {transform-origin: bottom center;transform: rotateX(-90deg);animation: fall 1.5s ease-in;}",
+        spin_page = "body{animation:spin 5s linear infinite;}",
+        no_final = [image_flip, spin_image, comic_sans, foggy_text, invisible_selection],
+        yes_final = [upside_down, fall, spin_page];
+
+    function getRandom(min, max){return  Math.floor(Math.random()*(max-min)+min);}
+
+    var styles = [];
+    if (getRandom(0, 2)) {
+        styles.push(yes_final[getRandom(0, yes_final.length)]);
+    } else {
+        var howMany = getRandom(2, 4);
+        do {
+            var index = getRandom(0, yes_final.length);
+            styles.push(no_final[index]);
+            styles = styles.filter(function(el, idx){if (idx !== index) {return el;}});
+        } while (styles.length < howMany);
+    }
+
+    var css = _default + styles.join("");
+    console.log(css);
+
+    var s=document.createElement('style');
+    s.innerHTML = css;
+    document.getElementsByTagName('head')[0].appendChild(s);
+
+    return css;
+}
+
 function main () {
     var dateObject = new Date(),
         day = dateObject.getDate(),
         month = dateObject.getMonth()+1,
         year = dateObject.getFullYear();
 
-    if (day == 14 && month == 2) {valentines();}  //san valentin
-    if (day == 11 && month == 9) {catalonia();}  //diada de catalunya
+    if (day == 14 && month ==  2) {valentines();}  //san valentin
+    if (day == 11 && month ==  9) {catalonia();}  //diada de catalunya
+    if (day ==  1 && month ==  4) {aprilFools();}  //sants innocents
 }
 
 module.exports.catalonia = catalonia;
 module.exports.valentines = valentines;
+module.exports.aprilFools = aprilFools;
 module.exports.main = main;
